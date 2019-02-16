bc593c286fa7c2aedf030cf634865c85f297acb5
Maksimillian <flomko.ru@gmail.com>
2019-02-16 19:16:33 +0300

    working populate db

--- mongo_connector/codebase/api_server.py
+++ mongo_connector/codebase/api_server.py
@@ -3,7 +3,7 @@ from flask import request, url_for
 #from flask_cors import CORS
 from flask_api import FlaskAPI, status, exceptions
 import json
-
+import db_helper
 
 app = FlaskAPI(__name__)
 
@@ -12,8 +12,19 @@ app = FlaskAPI(__name__)
 def search():
     profile = request.form.get("profile")
     image = request.form.get("image")
-
     return json.dumps(response)
+
+
+@app.route('/populate/',methods=['POST'])
+def insert():
+    print("Posted data : {}".format(request.get_json()))
+    # return "Hello World!"
+    data = request.get_json()
+    print(data)
+    data_new = db_helper.read_request(data)
+    res = db_helper.write_record(data_new)
+    return res
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- mongo_connector/codebase/db_helper.py
+++ mongo_connector/codebase/db_helper.py
@@ -17,22 +17,20 @@ formatted_req={
 
 search_reg= {"pet":"dog"}
 
-def connect():
-    # Connect to db, return obj of connection
-    client =  pymongo.mongo_client.MongoClient('mongodb://root:rootPassXXX@localhost:27017/admin')
-    db = client.db
-    return db
+# def connect():
+# Connect to db, return obj of connection
+client =  pymongo.mongo_client.MongoClient('mongodb://root:rootPassXXX@localhost:27017/admin')
+db = client.db
 
 def read_request(json_req):
     # Get json data, convert to mongo insert
-    return None
+    return json_req
 
-
-def write_record(formatted_reg):
+def write_record(formatted_req):
     # write bson to db, return res
     req = [pymongo.InsertOne(formatted_req)]
     res = db.test.bulk_write(req)
-    return res
+    return res.inserted_count 
 
 def search_record(search_req):
     # return result from spec collection
