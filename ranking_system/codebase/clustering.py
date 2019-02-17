d3964e6d898d681c30371c26c7f2d15f676e1f2e
Maksimillian <flomko.ru@gmail.com>
2019-02-17 17:51:13 +0300

    stale all work, trying to find way to clusterization

--- ranking_system/codebase/clustering.py
+++ ranking_system/codebase/clustering.py
@@ -4,33 +4,38 @@ import numpy as np
 from sklearn.cluster import KMeans
 import os
 from sklearn.neighbors import NearestNeighbors
+from sklearn.decomposition import PCA
+import pandas as pd
 
 
 extractor = cv2.xfeatures2d.SIFT_create()
+pca = PCA(n_components=1)
 
 images = os.listdir(path='.')
 image_path = '0t8wjdjFbX8.jpg'
 
 
-descriptor_list = []
+descriptor_list = np.empty(0)
 
 
 
 def features(image, extractor):
-    keypoints, descriptors = extractor.detectAndCompute(img, None)
+    keypoints, descriptors = extractor.detectAndCompute(image, None)
     return keypoints, descriptors
 
-for image in images:
-    img = cv2.imread(image)
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, descriptor = features(gray_image, extractor)
-    descriptor_list.append(descriptor)
+def get_descriptors(images):
+    for image in images:
+        img = cv2.imread(image)
+        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, descriptor = features(gray_image, extractor)
+        principalComponents = pca.fit_transform(descriptor)
+        np.append(descriptor_list,principalComponents)
+    return descriptor_list
 
-print(descriptor_list)
 
-kmeans = KMeans(n_clusters = 800)
+## need pca
+kmeans = KMeans(n_clusters = 128)
 kmeans.fit(descriptor_list)
-
 
 
 preprocessed_image = []
