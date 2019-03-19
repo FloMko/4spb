16d17bd9c73ab7b5d29a67703d989f8b38feb21b
Maksimillian <flomko.ru@gmail.com>
2019-03-20 02:28:17 +0300

    refactor clustering

--- ranking_system/codebase/cluster.py
+++ ranking_system/codebase/cluster.py
@@ -5,29 +5,32 @@ from keras.engine import Model
 from keras.preprocessing import image
 from sklearn.neighbors import NearestNeighbors
 
-import os
-
-
-dataset_path='./ranking_system/dataset/'
-images = os.listdir(path=dataset_path)
-
-bm = VGG19(weights='imagenet')
-model = Model(inputs=bm.input, outputs=bm.get_layer('fc1').output)
-
-def get_vector(path):
-    img = image.load_img(path, target_size=(224, 224)) # чтение из файла
-    x = image.img_to_array(img)  # сырое изображения в вектор
-    x = np.expand_dims(x, axis=0)  # превращаем в вектор-строку (2-dims)
-    x = preprocess_input(x) #  библиотечная подготовка изображения
-    vec = model.predict(x).ravel()
-    return vec
+class Cluster:
+    def __init__(self, paths):
+        self.bm = VGG19(weights='imagenet')
+        self.model = Model(inputs=self.bm.input, outputs=self.bm.get_layer('fc1').output)
+        self.paths = paths
+        print('Init cluster')
 
 
 
-def main():
-    vec = get_vector(dataset_path+images[0])
-    print(vec)
-    print(vec.shape)
+    def get_vector(self, path):
+        # read from file
+        img = image.load_img(path, target_size=(224, 224))
+        # make vector
+        x = image.img_to_array(img)
+        # transform vector to one-dimension
+        x = np.expand_dims(x, axis=0)
+        # preprocesing by library
+        x = preprocess_input(x)
+        vec = self.model.predict(x).ravel()
+        return vec
+
+    def copy_data(self, data):
+        '''
+        get array of images, return copy with additional vector
+        '''
+        print(data.shape)
 
 if __name__ == "__main__":
-    main()
+    Cluster.init()
