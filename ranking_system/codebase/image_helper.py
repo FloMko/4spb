53c568f345990f0c3424667d27e13e409275fd0b
Maksimillian <flomko.ru@gmail.com>
2019-03-20 02:06:58 +0300

    refactor Helper to Class, init modyle for fixing to full path

--- ranking_system/codebase/image_helper.py
+++ ranking_system/codebase/image_helper.py
@@ -2,23 +2,36 @@
 import cv2
 # for basic load/save
 import os
-dataset_path='./ranking_system/dataset/'
-images = os.listdir(path=dataset_path)
 
+class Helper:
+    '''
+    for resiuzing image for imagenet
+        fixing path to dynamic directory
+    '''
+    def __init__(self, images, dataset_path):
+        self.dataset_path = dataset_path
+        self.images = images
+        print('init helper')
 
-def resize(images, image_path, dim=(224,224)):
-    for image in images:
-        print(image)
-        img = cv2.imread(image_path+image, cv2.IMREAD_UNCHANGED) 
-        print('Original Dimensions : ',img.shape) 
-        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        cv2.imwrite(image_path+image, resized) 
-        img = cv2.imread(image_path+image, cv2.IMREAD_UNCHANGED) 
-        print('resized Dimensions : ',img.shape) 
+    def resize(self, images, dim=(224,224)):
+        for image in self.images:
+            print(image)
+            img = cv2.imread(image, cv2.IMREAD_UNCHANGED) 
+            print('Original Dimensions : ',img.shape) 
+            resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+            cv2.imwrite(image, resized) 
+            img = cv2.imread(image, cv2.IMREAD_UNCHANGED) 
+            print('resized Dimensions : ',img.shape)
 
-
-def main():
-    resize(images, dataset_path)
+    def fix_path(self):
+        '''
+        return formated path as np.array
+        '''
+        full_paths = list()
+        for img in self.images:
+            full_paths.append(self.dataset_path + img)
+        self.images = full_paths
+        return self.images
 
 if __name__ == "__main__":
-    main()
+    Helper.init()
