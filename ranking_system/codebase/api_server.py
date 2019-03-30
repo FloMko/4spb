2b6ca2c65f2999f1056f93eb388b6960370c8d1d
Maksimillian <flomko.ru@gmail.com>
2019-03-30 16:00:10 +0300

    init populate database with transform data

--- ranking_system/codebase/api_server.py
+++ ranking_system/codebase/api_server.py
@@ -9,21 +9,19 @@ import db_helper
 mongourl = 'mongodb://root:rootPassXXX@127.0.0.1:27017/admin'
 database = 'lostpets'
 collection = 'dataset'
+collection_new= 'datastore'
 
 
 app = FlaskAPI(__name__)
 db = db_helper.db(mongourl,database,collection)
+db_new = db_helper.db(mongourl,database,collection_new)
 
-
-def toJson(data):
-    """Convert Mongo object(s) to JSON"""
-    return json.dumps(data, default=json_util.default)
 
 @app.route('/search/',methods=['POST'])
 def search():
-    '''
+    """
     get request, search in db for data
-    '''
+    """
     search_list = list()
     data = request.get_json()
     response = db.search_records(data)
@@ -31,13 +29,34 @@ def search():
 
 @app.route('/populate/',methods=['POST'])
 def insert():
-    '''
+    """
     Get POST json. populate db
-    '''
+    """
     data = request.get_json()
     # data_new = db.read_request(data)
     res = db.write_record(data)
     return str(res)
+
+@app.route('/search_trans/',methods=['POST'])
+def search_trans():
+    """
+    get request, search in db for data
+    """
+    search_list = list()
+    data = request.get_json()
+    response = db_new.search_records(data)
+    return json_util.dumps(response)
+
+@app.route('/populate_trans/',methods=['POST'])
+def insert_trans():
+    """
+    Get POST json. populate db
+    """
+    data = request.get_json()
+    # data_new = db.read_request(data)
+    res = db_new.write_record(data)
+    return str(res)
+
 
 
 if __name__ == "__main__":
--- ranking_system/codebase/db_transform.py
+++ ranking_system/codebase/db_transform.py
@@ -1,11 +1,13 @@
 import requests
 import ast
 
-pop_url = 'http://127.0.0.1:5000/populate/'
+search_url = 'http://127.0.0.1:5000/search/'
+pop_url = 'http://127.0.0.1:5000/populate_trans/'
 res = requests.post(pop_url, json={})
 
 result_list = []
 rec_dict = {}
+i = 0
 
 
 def get_old_db():
@@ -13,11 +15,10 @@ def get_old_db():
     make request to database
     :return: data
     """
-    search_url = 'http://127.0.0.1:5000/search/'
     req = requests.post(search_url, json={})
     data = ast.literal_eval(req.text)
-    data = data[0]['response']['items']
-    return data
+    data_new = data[0]['response']['items']
+    return data_new
 
 
 def transform_data(data):
@@ -25,21 +26,33 @@ def transform_data(data):
     :param data: get mongodb dict
     :return: structured dict with {rec['id']:{ 'ownerid': rec['owner_id'], 'photos': list_photo}}
     """
-
+    i = 0
     for rec in data:
-        list_photo = []
+        photos = []
+        photos_name = []
         if 'attachments' in rec.keys():
             attachments_list = rec['attachments']
             for attach in attachments_list:
                 if attach['type'] == 'photo':
-                    list_photo.append(attach['photo']['photo_604'])
-            rec_dict.update({rec['id']: {'ownerid': rec['owner_id'], 'photos': list_photo}})
+                    i = i + 1
+                    photos.append(attach['photo']['photo_604'])
+                    photos_name.append(attach['photo']['photo_604'].split('/')[-1])
+                    rec_dict.update({i: {'postid': rec['id'], 'ownerid': rec['owner_id'], 'photos_url': photos,
+                                         'photos_name': photos_name}})
     return rec_dict
+
+
+def populate_data(data):
+    for req in data:
+        print(data[req])
+        res = requests.post(pop_url, json=data[req])
+    return res
 
 
 if __name__ == "__main__":
     data = get_old_db()
-    print(transform_data(data))
+    data_new = (transform_data(data))
+    populate_data(data_new)
 
 # how-to build db schema
 # vk_wall_identificator = 'https://vk.com/wall'+'owner_id' + '_' + 'id'
