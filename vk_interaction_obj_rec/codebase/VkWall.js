01c3f8169861beb7edcd1eeb9ae059c943664898
Anton Fabritsius <antonfabritsius@gmail.com>
2019-10-12 17:54:08 +0300

    don't send duplicate posts to the database

--- vk_interaction_obj_rec/codebase/VkWall.js
+++ vk_interaction_obj_rec/codebase/VkWall.js
@@ -1,10 +1,8 @@
-const vk = require('vk-easy');
-require('dotenv').load();
-const axios = require('axios');
-const _ = require('underscore');
+const vk = require('vk-easy')
+require('dotenv').load()
+const axios = require('axios')
 var url = 'http://api-python:5000' // url базы данных Максима
 var BOT_TOKEN = process.env.BOT_TOKEN
-var DATA = {}
 
 
 function getWall() {
@@ -12,16 +10,30 @@ function getWall() {
     access_token: BOT_TOKEN,
     domain: 'yapoteryalsyaspb',
     count: 50,
-  }, true).then(res => {
-    sendToDb(res)
+  }, true).then(async res => {
+    
+    try {
+      const margin = await getLatestDate()
+
+      for (let wallPost of res.response.items) {
+        if (wallPost.date > margin) {
+          sendToDb(wallPost)
+        } else {
+          console.log(`post ${wallPost.id} already exists`)
+        }
+      }
+    } catch(err) {
+      console.log(err)
+    }
+
   });
 }
 
 
 function sendToDb(data) {
-  axios.post(url+'/populate/', data)
+  axios.post(`${url}/populate/`, data)
   .then(function (response) {
-    console.log(response);
+    console.log(`${data.id} – ${response.status}`)
   })
   .catch(function (error) {
     console.log(error);
@@ -29,5 +41,13 @@ function sendToDb(data) {
 }
 
 
-getWall()
+function getLatestDate() {
+  return axios.get(`${url}/get_latest`)
+  .then((response) => {
+    return response.data
+  })
+  .catch((err) => 0)
+}
 
+
+getWall()
