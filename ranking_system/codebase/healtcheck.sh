50c8806dc660a65445562dbcac09fdfc0b41c990
maxim.ermolenko <flomko.ru@gmail.com>
2019-10-26 02:43:33 +0300

    set ordering docker-compose

---
+++ ranking_system/codebase/healtcheck.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo Testing if $1 is UP
+
+http_code=$(curl -LI $1 -o /dev/null -w '%{http_code}\n' -s)
+
+if [ ${http_code} -eq 404 ]; then
+    echo The service is UP and Running
+    exit 0
+else
+    echo The service is DOWN
+    exit 1
+fi
+
+The problem I'm f
