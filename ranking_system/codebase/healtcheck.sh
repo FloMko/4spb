4ee510ef508b2a3505f9a9d68aa6bd48dc0c2e20
maxim.ermolenko <flomko.ru@gmail.com>
2019-10-27 18:49:44 +0300

    fix healthcheck

--- ranking_system/codebase/healtcheck.sh
+++ ranking_system/codebase/healtcheck.sh
@@ -3,7 +3,7 @@ echo Testing if $1 is UP
 
 http_code=$(curl -LI $1 -o /dev/null -w '%{http_code}\n' -s)
 
-if [ ${http_code} -eq 404 ]; then
+if [ ${http_code} -eq 405 ]; then
     echo The service is UP and Running
     exit 0
 else
